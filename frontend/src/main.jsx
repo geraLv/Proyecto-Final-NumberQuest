1dd5707ecdab92122f9d728ee15abe2101d88c92
gerardo vega <gerardoluisvega@gmail.com>
2024-09-30 14:15:31 -0300

    branch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,9 @@ import App from "./App.jsx";
 // import LoginForm from "./components/loginForm.jsx";
 // import RegisterForm from "./components/registerForm.jsx";
 import "./index.css";
-import ListaTemas from "./lista-temas.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD:src/main.jsx
-    {/* <App /> */}
-    <ListaTemas></ListaTemas>
-=======
-    <App/>
->>>>>>> 025e614da9c8f49af148a89843b76d09b7770b0d:frontend/src/main.jsx
+    <App />
   </StrictMode>
 );
