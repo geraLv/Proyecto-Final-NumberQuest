e3d129b833454c17c52f6c71377b153c56b62ffd
Luca Gabriel Espinoza <luca.g.esp2005@gmailcom>
2024-09-30 14:07:00 -0300

    ...

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,12 @@ import App from "./App.jsx";
 // import LoginForm from "./components/loginForm.jsx";
 // import RegisterForm from "./components/registerForm.jsx";
 import "./index.css";
-import ListaTemas from "./lista-temas.jsx";
+// import ListaTemas from "./lista-temas.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD:src/main.jsx
-    {/* <App /> */}
-    <ListaTemas></ListaTemas>
-=======
+    <App />
+    {/* <ListaTemas></ListaTemas> */}
     <App/>
->>>>>>> 025e614da9c8f49af148a89843b76d09b7770b0d:frontend/src/main.jsx
   </StrictMode>
 );
