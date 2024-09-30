f5f0d50bb0cb318235971683035ea257f8d4b7a1
gerardo vega <gerardoluisvega@gmail.com>
2024-09-30 16:24:52 -0300

    Detalles al generar una rama

---
+++ frontend/src/components/bloques.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+const Bloques = () => {
+  return (
+    <div>
+      <section class="text-gray-400 body-font flex justify-center">
+        <div class="container mx-1/2">
+          <div class="flex flex-wrap -m-4">
+            <div class="xl:w-1/4 md:w-1/2 px-4 py-4">
+              <div class="bg-brandDark bg-opacity-90 p-6 rounded-lg">
+                <img
+                  class="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 class="text-lg text-white font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="bg-brandDark bg-opacity-90 p-6 rounded-lg">
+                <img
+                  class="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 class="text-lg text-white font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="bg-brandDark bg-opacity-90 p-6 rounded-lg">
+                <img
+                  class="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 class="text-lg text-white font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="bg-brandDark bg-opacity-90 p-6 rounded-lg">
+                <img
+                  class="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 class="text-lg text-white font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Bloques;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: "#2b4c7e",
+        secondary: "#606d80",
+        brandDark: "#606d80",
+        dark: "#1f1f20",
+        light: "#dce0e6",
+      },
+    },
+    plugins: [],
+  },
+});
