cc41f9b37fee6ae5a80c86e36fdf0b2f943c2504
Gerardo Vega <gerardoluisvega@gmail.com>
2024-09-30 22:04:10 -0300

    ala

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ListaTemas from "./lista-temas.jsx";
+// import ListaTemas from "./lista-temas.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <ListaTemas></ListaTemas>
+    <App />
+    {/* <ListaTemas></ListaTemas> */}
   </StrictMode>
 );
