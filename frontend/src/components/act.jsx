e31dffd660035f9e61695b497bfe00f8748ca9e7
Luca Gabriel Espinoza <luca.g.esp2005@gmailcom>
2024-10-02 14:44:17 -0300

    Se mejoró la validación de la actividad y se agregó un punto extra

--- frontend/src/components/act.jsx
+++ frontend/src/components/act.jsx
@@ -3,29 +3,44 @@ import React, { useState } from 'react';
 export const Actividad = () => {
     const [parte1, setParte1] = useState('');
     const [parte2, setParte2] = useState('');
+    const [parte1B, setParte1B] = useState('');
+    const [parte2B, setParte2B] = useState('');
     const [envio, setEnvio] = useState('');
-    const [mostrarInputs, setMostrarInputs] = useState(true); // Para la primera actividad
+    const [envioB, setEnvioB] = useState('');
+    const [mostrarInputsA, setMostrarInputsA] = useState(true);
+    const [mostrarResultadoA, setMostrarResultadoA] = useState(false);
+    const [mostrarInputsB, setMostrarInputsB] = useState(false);
+    const [mostrarResultadoB, setMostrarResultadoB] = useState(false);
+    const [mostrarB, setMostrarB] = useState(false);
+    const [mostrarA, setMostrarA] = useState(true);
 
-    const answers = [
-        '-4 ; 4',
-        '1 ; 3.4'
-    ]; //Array con los Dominios correctos de las funciones
-
+    const answers = ['-4 ; 4', '-inf,0 U 1/4,+inf'];
     const imgRes = [
-        <img src="https://th.bing.com/th/id/OIP.OFQpZjI_YOwDZoYnXh1VzwHaD_?rs=1&pid=ImgDetMain" alt="Incorrecto" />,
-        <img src="https://th.bing.com/th/id/OIP.2IU--_lJKHd3j90ik90CfgHaFj?w=265&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Correcto" />
+        <img style={{ width: 560, height: 292 }} src="https://th.bing.com/th/id/OIP.k6H2Q8P1DKlgmf4tMLFwvgHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1,3" alt="" />,
+        <img src="https://media.licdn.com/dms/image/C5112AQEYARonHijMaQ/article-cover_image-shrink_600_2000/0/1549770046623?e=2147483647&v=beta&t=9B-KDNvL_mfs2VrOXKrsHf8T5NMG2MXAKCi5i89cXeE" alt="" />
     ];
 
-    // Manejo de la primera actividad
     const handleParte1Change = (input) => {
-        if (/^-?\d*\.?\d*$/.test(input)) { // Acepta números negativos y decimales
+        if (/^-?\d*\.?\d*$/.test(input)) {
             setParte1(input);
         }
     };
 
     const handleParte2Change = (input) => {
-        if (/^-?\d*\.?\d*$/.test(input)) { // Acepta números negativos y decimales
+        if (/^-?\d*\.?\d*$/.test(input)) {
             setParte2(input);
+        }
+    };
+
+    const handleParte1BChange = (input) => {
+        if (/^-?\d*\.?\d*$/.test(input)) {
+            setParte1B(input);
+        }
+    };
+
+    const handleParte2BChange = (input) => {
+        if (/^-?\d*\.?\d*$/.test(input)) {
+            setParte2B(input);
         }
     };
 
@@ -35,59 +50,140 @@ export const Actividad = () => {
         } else {
             const respuesta = `${parte1} ; ${parte2}`;
             setEnvio(respuesta);
-            setMostrarInputs(false); // Oculta los inputs al responder
+            setMostrarResultadoA(true);
+            setMostrarInputsA(false);
+        }
+    };
+
+    const handleSubmitB = () => {
+        if (parte1B === '' || parte2B === '' || isNaN(parte1B) || isNaN(parte2B)) {
+            alert("Error: Debe ingresar un número válido");
+        } else {
+            const respuestaB = `${parte1B} U ${parte2B}`;
+            setEnvioB(respuestaB);
+            setMostrarResultadoB(true);
+            setMostrarInputsB(false);
         }
     };
 
     const validar = envio === answers[0] ? 'Correcto' : 'Incorrecto';
+    const validarB = envioB === answers[1] ? 'Correcto' : 'Incorrecto';
+
+    const handleNext = () => {
+        setMostrarB(true);
+        setMostrarResultadoA(false);
+        setMostrarA(false);
+        setMostrarResultadoB(true); // Oculta el resultado de B
+        setMostrarInputsB(true); // Oculta los inputs de B
+    };
 
     return (
         <div className="main">
             <h1 style={{ fontSize: 30 }}><b>Dominio de funciones</b></h1>
-            <p style={{ color: '#AABFC6', marginTop: '10px' }}>*Aclaración: Los números decimales se marcan con punto: "1.2"*</p>
+            <p style={{ color: '#AABFC6', marginTop: '10px' }}>*Aclaración: Los números decimales se marcan con punto: Ejemplo:"1.2"*</p>
 
             <div className='Act1' style={{ margin: '10px 10px 30px 10px' }}>
                 <p style={{ fontSize: 20, marginBottom: '10px' }}>Actividad 1:</p>
-                <div style={{ marginBottom: '10px' }}>A) <img src="../../public/img/funcion1.png" alt="Límite gráfico" /></div>
-
-                {mostrarInputs && (
-                    <p style={{ fontSize: 17 }}>A) Indica el dominio de la función =
-                        <div className="" style={{ marginTop: '10px' }}>
-                            <input
-                                type="text"
-                                onChange={(e) => handleParte1Change(e.target.value)}
-                                placeholder="(-x.x)"
-                                maxLength="4"
-                                style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
-                            />
-                            <span style={{ fontSize: 20, margin: '0px 5px 9px 5px' }}>;</span>
-                            <input
-                                type="text"
-                                onChange={(e) => handleParte2Change(e.target.value)}
-                                placeholder="(-x.x)"
-                                maxLength="4"
-                                style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
-                            />
-                            {mostrarInputs && (
-                                <button onClick={handleSubmit} style={{
-                                    backgroundColor: '#AABFC6',
-                                    color: '#000',
-                                    marginLeft: '10px',
-                                    padding: '5px',
-                                    border: 'solid, black, 2px',
-                                    borderRadius: '5px',
-                                }}>Responder</button>
-                            )}
-                        </div>
-                    </p>
+                {mostrarA && (
+                    <>
+                        <div style={{ marginBottom: '10px' }}>A) <img src="../../public/img/funcion1.png" alt="" /></div>
+                        <p style={{ fontSize: 17 }}> Indica el dominio de la función =
+                            <div className="" style={{ marginTop: '10px' }}>
+                                {mostrarInputsA && (
+                                    <>
+                                        <input
+                                            type="text"
+                                            onChange={(e) => handleParte1Change(e.target.value)}
+                                            placeholder="(-x.x)"
+                                            maxLength="4"
+                                            style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
+                                        />
+                                        <span style={{ fontSize: 20, margin: '0px 5px 9px 5px' }}>U</span>
+                                        <input
+                                            type="text"
+                                            onChange={(e) => handleParte2Change(e.target.value)}
+                                            placeholder="(-x.x)"
+                                            maxLength="4"
+                                            style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
+                                        />
+                                        <button onClick={handleSubmit} style={{
+                                            backgroundColor: '#AABFC6',
+                                            color: '#000',
+                                            marginLeft: '10px',
+                                            padding: '5px',
+                                            border: 'solid, black, 2px',
+                                            borderRadius: '5px',
+                                        }}>Responder</button>
+                                    </>
+                                )}
+                            </div>
+                        </p>
+                    </>
                 )}
 
-                {envio && (
+                {mostrarResultadoA && envio && (
                     <div>
                         <p style={{ color: validar === 'Correcto' ? 'green' : 'red', fontSize: 18 }}>
                             Dominio: ( {envio} ) es {validar}
                         </p>
                         <p>{validar === 'Correcto' ? imgRes[1] : imgRes[0]}</p>
+                    </div>
+                )}
+
+                {!mostrarB && envio && (
+                    <button onClick={handleNext} style={{
+                        backgroundColor: '#AABFC6',
+                        color: '#000',
+                        marginTop: '10px',
+                        padding: '5px',
+                        border: 'solid, black, 2px',
+                        borderRadius: '5px',
+                    }}>Siguiente punto</button>
+                )}
+
+                {/* Sección B */}
+                {mostrarB && (
+                    <div style={{ marginTop: '30px' }}>
+                        <p style={{ fontSize: 20, marginBottom: '10px' }}>B) <img src="../../public/img/funcion2.png" alt="" />Indica el dominio de la función = 
+                            <div className="" style={{ marginTop: '10px' }}>
+                                {mostrarInputsB && (
+                                    <>
+                                        <input
+                                            type="text"
+                                            onChange={(e) => handleParte1BChange(e.target.value)}
+                                            placeholder="(-x.x)"
+                                            maxLength="8"
+                                            style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
+                                        />
+                                        <span style={{ fontSize: 20, margin: '0px 5px 9px 5px' }}>;</span>
+                                        <input
+                                            type="text"
+                                            onChange={(e) => handleParte2BChange(e.target.value)}
+                                            placeholder="(-x.x)"
+                                            maxLength="8"
+                                            style={{ width: '70px', padding: '3px', backgroundColor: '#D9DDDE', borderRadius: '5px', border: 'solid, black, 2px' }}
+                                        />
+                                        <button onClick={handleSubmitB} style={{
+                                            backgroundColor: '#AABFC6',
+                                            color: '#000',
+                                            marginLeft: '10px',
+                                            padding: '5px',
+                                            border: 'solid, black, 2px',
+                                            borderRadius: '5px',
+                                        }}>Responder</button>
+                                    </>
+                                )}
+                            </div>
+                        </p>
+
+                        {mostrarResultadoB && envioB && (
+                            <div>
+                                <p style={{ color: validarB === 'Correcto' ? 'green' : 'red', fontSize: 18 }}>
+                                    Dominio: ( {envioB} ) es {validarB}
+                                </p>
+                                <p>{validarB === 'Correcto' ? imgRes[1] : imgRes[0]}</p>
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
