225b283f0f1ecb9592780ebf955bf6ca8774a69e
gerardo vega <gerardoluisvega@gmail.com>
2024-10-03 09:09:47 -0300

    Cambios en el carrucel

--- frontend/src/components/carusel.jsx
+++ frontend/src/components/carusel.jsx
@@ -1,24 +1,25 @@
 import { Carousel } from "@material-tailwind/react";
 import React from "react";
-import Banner1 from "../../public/img";
+// import { Limite } from "";
+
 export function CarouselDefault() {
   return (
     <div className="../../public/board-5599231.png">
       <Carousel className="bg-primary">
         <img
-          src="https://www.canva.com/design/DAGSebDRnJs/zYCZWvXlUtXkDYTdZ6RCkw/edit?utm_content=DAGSebDRnJs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+          src="../../public/img/Banner1.svg"
           alt=""
-          className="h-96 w-full object-cover"
+          className="h-100 w-full object-cover"
         ></img>
         <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          src="../../public/img/Banner2.svg"
           alt="image 2"
-          className="h-96 w-full object-cover"
+          className="h-full w-full object-cover"
         />
         <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          src="../../public/img/Banner3.svg"
           alt="image 3"
-          className="h-96 w-full object-cover"
+          className="h-full w-auto object-cover"
         />
       </Carousel>
     </div>
