bd01273454859a1a6cbf7633061b135e8eb438da
Luca Gabriel Espinoza <luca.g.esp2005@gmailcom>
2024-10-15 12:27:46 -0300

    Se comienza a realizar mas actividades

--- frontend/src/components/activities.jsx
+++ frontend/src/components/activities.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "../index.css";
 import NavBar from "./navbar";
-import { Actividad } from "./act";
+import { Actividad } from "../activities/act";
 import Layaut from "./layaut";
 import Teclado from "./teclado";
 // import { Navbar } from "@material-tailwind/react";
 import Footer from "./footer";
+import { Composición1 } from "../activities/comp";
 
 export function Activities() {
   return (
     <>
       <NavBar />
       <Layaut>
-        <Actividad />
+        <Composición1/>
       </Layaut>
       <Teclado></Teclado>
       <Footer></Footer>
--- frontend/src/components/teclado.jsx
+++ frontend/src/components/teclado.jsx
@@ -5,7 +5,8 @@ const Teclado = () => {
     <div className="bg-gray-50 h-full w-full container ">
       Teclado
       <div className="grid grid-cols-4 grid-rows-4 h-full m-10 gap-1">
-        <button className="flex shadow-lg h-20 rounded-tl-lg justify-center text-3xl items-center bg-white hover:scale-105 hover:shadow-2xl duration-100  ease-in">
+        <button className="flex shadow-lg h-20 rounded-tl-lg justify-center text-3xl items-center bg-white hover:scale-105 hover:shadow-2xl duration-100  ease-in"
+        onClick={e=>{}}>
           1
         </button>
         <button className="flex shadow-lg  justify-center text-3xl items-center bg-white hover:scale-105 duration-100 ease-in">
