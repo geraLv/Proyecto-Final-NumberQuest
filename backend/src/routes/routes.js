b2e9f9e3437d013d4254a40d9b9e2c71c2f9f21c
gerardo vega <gerardoluisvega@gmail.com>
2024-10-30 16:46:51 -0300

    https://github.com/geraLv/Proyecto-Final-NumberQuest.git

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -14,11 +14,14 @@ import {
   validaciones,
 } from "../middlewares/express-validator.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
+import { apiUsers, apiUser } from "../models/api.js";
 
 // Rutas
 export const userRoutes = Router();
 
 userRoutes.get("/session", validarJWT, session);
+userRoutes.get("/api/users", apiUsers);
+userRoutes.get("/api/user/*", apiUser);
 userRoutes.post("/login", login);
 userRoutes.post("/logout", cerrarSesion);
 userRoutes.post("/register", validaciones, aplicarValidaciones, register);
